2194e2abd24faf8c0ea3ccc3b54246277111b1d8
Max <shelbyrusky@gmail.com>
2019-03-04 02:16:22 +0300

    Третий комммит

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,30 +1,21 @@
 
 var budget = prompt("Какой у вас бюджет?",1000);
 var name = prompt("Название вашей фирмы?","У Ашота");
-/*console.log(budget);
-console.log(name);*/
 
-MainList = {
+let MainList = {
 		budget: budget,
 		name: name,
 		shopGoods: [],
 		employers: {}, 
 		open: true
 }
-console.log(MainList["name"]);
-MainList.shopGoods[0] = prompt("Какой тип товаров будем производить?",'Тип товара');
-console.log(MainList.shopGoods[0]);
-MainList.shopGoods[1] = prompt("Какой тип товаров будем производить?",'Тип товара');
-console.log(MainList.shopGoods[1]);
-MainList.shopGoods[2] = prompt("Какой тип товаров будем производить?",'Тип товара');
-console.log(MainList.shopGoods[2]);
+
+for (let i = 0; i < 5; i++) {
+	MainList.shopGoods[i] = prompt("Какой тип товаров будем производить?",'Тип товара');
+	console.log(MainList.shopGoods[i]);
+}
+
+
+
 budget = budget/30;
 alert("Бюджет вашей фирмы на 1 день " + budget + '$');
-
-
-var num = 33721;
-var a = num % 10; //1
-var j = num - a; //33720
-var	b = num % 100; //2
-var h = j - (b * 10); //337
-var y = h % 10; //7
